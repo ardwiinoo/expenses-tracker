f7194f3177860c718b4857f049fb42652728f8d1
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-05-04 20:14:34 +0700

    chore: add generateVerificationCode utility function

---
+++ src/utils/generateCode.ts
@@ -0,0 +1,8 @@
+export function generateVerificationCode(length: number = 6): string {
+    const digits = '0123456789'
+
+    return Array.from(
+        { length },
+        () => digits[Math.floor(Math.random() * digits.length)]
+    ).join('')
+}

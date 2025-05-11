a3b31220c663ce774860dddd0d0ec3373eac4e6f
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-05-11 18:49:53 +0700

    feat: create SignInPage component to render SignInForm

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,9 @@
+import SignForm from './SignInForm'
+
+export default function SignInPage() {
+    return (
+        <main className="flex min-h-screen items-center justify-center">
+            <SignForm />
+        </main>
+    )
+}
